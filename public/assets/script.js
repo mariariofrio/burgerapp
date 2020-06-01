b2fd07046cc6ef905ab8fae4780e7050cda7242f
majo <maria.riofrio.mr@gmail.com>
2020-06-01 14:26:31 -0400

    current work

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,1 +1,62 @@
 console.log ("css is a file")
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-sleep").on("click", function(event) {
+      var id = $(this).data("id");
+      var newBurger = $(this).data("newBurger");
+  
+      var newBurgerState = {
+        type: newBurger
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newBurgerState
+      }).then(
+        function() {
+          console.log("changed sleep to", newBurger);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurgers = {
+        name: $("#ca").val().trim(),
+        sleepy: $("[name=sleepy]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurgers
+      }).then(
+        function() {
+          console.log("created new cat");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-cat").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted burger", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
